52482f97e267169932090e18ad74750b57c26a01
juliansa99 <saaljulian@gmail.com>
2026-03-29 14:31:00 -0300

    TP 1 COMPLETO

--- parte1.js
+++ parte1.js
@@ -46,3 +46,33 @@ console.log(productos.every(producto => producto.precio >= 1000));
 console.log("EJERCICIO 3.10")
 console.log(nombres.includes("Campera"));
 
+console.log("EJERCICIO 4.11")
+const ordenados = [...productos].sort((a, b) => a.precio - b.precio);
+console.log(ordenados);
+
+
+console.log("EJERCICIO 4.12")
+const mensajes = productos.map(producto =>
+  `El producto ${producto.nombre} cuesta $${producto.precio} y pertenece a la categoría ${producto.categoria}.`
+);
+
+console.log(mensajes);
+[
+  "El producto Remera cuesta $1500 y pertenece a la categoría Ropa.",
+  "El producto Pantalón cuesta $2500 y pertenece a la categoría Ropa",
+  "El producto Zapatillas cuesta $8000 y pertenece a la categoría Calzado",
+  "El producto Gorra cuesta $1200 y pertenece a la categoría Accesorios",
+  "El producto Campera cuesta $9500 y pertenece a la categoría Ropa"
+]
+
+console.log("EJERCICIO 4.13")
+const productos2 = [
+  { id: 6, nombre: "Buzo", precio: 2500, categoria: "Ropa" },
+  { id: 7, nombre: "Medias", precio: 500, categoria: "Ropa" },
+  { id: 8, nombre: "Ojotas", precio: 5000, categoria: "Calzado" },
+  { id: 9, nombre: "Reloj", precio: 2200, categoria: "Accesorios" },
+  { id: 10, nombre: "Camperón", precio: 12500, categoria: "Ropa" }
+]
+
+const combinado = [...productos, ...productos2]
+console.log(combinado)
